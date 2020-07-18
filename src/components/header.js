3e9d8033562607ce380235c7116cede2bb2870d1
raythompsonwebdev <ray_thomp@hushmail.com>
2020-07-18 13:35:45 +0100

    added mobile navigation script used useeffects hook - tidy up css

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,58 @@
 import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import $ from "jquery"
+//import $ from "jquery"
 
 class Header extends React.Component {
   constructor(props) {
     super(props)
     this.sitedesciption = props.siteDescription
     this.sitetitle = props.siteTitle
+  }
+
+  componentDidMount() {
+
+    var position, direction, previous
+
+    let menuToggle = document.getElementById("menu-toggle")
+
+    window.scroll(function() {
+      if (this.scrollTop() >= position) {
+        direction = "down"
+        if (direction !== previous) {
+          menuToggle.classList.add("hide")
+          previous = direction
+        }
+      } else {
+        direction = "up"
+        if (direction !== previous) {
+          menuToggle.classList.remove("hide")
+          previous = direction
+        }
+      }
+      position = this.scrollTop()
+    })
+
+    // sliding menu mobile
+    menuToggle.addEventListener("click", function(event) {
+      event.preventDefault()
+
+      // create menu variables
+      let slideoutMenu = document.querySelector("#main-nav ")
+      let slideoutMenuWidth = slideoutMenu.style.width
+
+      console.log(slideoutMenuWidth)
+
+      // toggle open class
+      slideoutMenu.classList.toggle("open")
+
+      // slide menu
+      if (slideoutMenu.classList.contains("open")) {
+        slideoutMenu.style.left = "0px"
+      } else {
+        slideoutMenu.style.left = slideoutMenuWidth
+      }
+    })
   }
 
   render() {
